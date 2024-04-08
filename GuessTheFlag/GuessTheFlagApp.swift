b1f9a6e02dc482de62ed4692ecb7a45d2bbc5832
rubythebelle <rubybellekim@gmail.com>
2024-04-08 14:49:34 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ruby Kim on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
